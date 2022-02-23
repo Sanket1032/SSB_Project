56946243f09dde87ca539ca7854ee4d998c6e3c1
Sanket1032 <sanketshinde1032@gmail.com>
2022-02-23 15:34:15 +0530

    Images added in slider

--- my-app/src/component/Landing/Slider/DataSlider.js
+++ my-app/src/component/Landing/Slider/DataSlider.js
@@ -2,13 +2,15 @@ import img1 from '../../../image/p1.jpg';
 import img2 from '../../../image/bg1.jpg';
 import img3 from '../../../image/bg2.jpg';
 import img4 from '../../../image/bg3.jpg';
-import img5 from '../../../image/bg4.jpg';
-
+import img5 from '../../../image/bg9.jfif';
+import img6 from '../../../image/bg12.jpg';
+import img7 from '../../../image/bg11.jpg';
+import img8 from '../../../image/bg6.jpg';
 const DataSlider=[
     {
         id:1,
         image:img1,
-        quote:"I won’t die in an accident or die of any disease. I will go down in glory."
+        quote:"I won’t die in an accident or die of any disease, I will go down in glory."
     },
     {
         id:2,
@@ -29,6 +31,21 @@ const DataSlider=[
         id:5,
         image:img5,
         quote: "We live by chance, we love by choice, we kill by profession."
+    },
+    {
+        id:6,
+        image:img6,
+        quote: "Some goals are so worthy, it’s glorious even to fail."
+    },
+    {
+        id:7,
+        image:img7,
+        quote: "We fight to win and win with a knock out,because there are no runners up in war."
+    },
+    {
+        id:8,
+        image:img8,
+        quote: "What is a lifetime adventure for you is a daily routine for us."
     },
 ]
 

5c13abf76feee855b11561b70ccf7911364381c9
Sanket1032 <sanketshinde1032@gmail.com>
2022-02-24 12:03:08 +0530

    Footer html Done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
 import Header from './component/Header/Header';
 import Tiranga from './image/tiranga-bg.jpg';
 import Landing from './component/Landing/Landing';
+import Footer from './component/Footer/Footer';
 import './App.css';
-const App=()=>{
-  return(
+const App = () => {
+  return (
     <div className="mainApp">
       <img src={Tiranga} className="main_img"></img>
       <div className="subApp">
         <Header />
         <Landing />
+        <Footer />
       </div>
     </div>
   );
--- my-app/src/component/Footer/Footer.js
+++ my-app/src/component/Footer/Footer.js
@@ -1,9 +1,27 @@
+import LogoImage from '../../image/logo_image.png';
+import LogoName from '../../image/logo_name.png';
 import './Footer.css';
 
-const Footer=()=>{
-    return(
+const Footer = () => {
+    return (
         <div className="Footer">
-            
+            <div className="upperFooter">
+                <div className="Footer_Logo">
+                    <img src={LogoImage} className="Logo_image"></img>
+                    <img src={LogoName} className="Logo_name"></img>
+                </div>
+                <div className="Footer_Form">
+                    <form>
+                        <input id="Name" className="form_input" type="text" placeholder="Name"></input>
+                        <input id="Email" className='form_input' type="email" placeholder="Email"></input>
+                        <textarea className="form_input" id="textarea" col="5" rows="5" placeholder="Type your message here..."></textarea>
+                    </form>
+                </div>
+            </div>
+            <div className="lowerFooter">
+                <p className="Disclaimer">Hey..You are looking at this masterpiece. So if you like this website, give the owner a peri peri treat ;).</p>
+                <p className="copyright">All Rights Reserved</p>
+            </div>
         </div>
     );
 };

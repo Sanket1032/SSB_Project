97232f1d8615b48a50d11273b659dcb909355755
Sanket1032 <sanketshinde1032@gmail.com>
2022-02-25 11:27:14 +0530

    ssb data in progress

---
+++ my-app/src/component/Ssb/Ssb.js
@@ -0,0 +1,14 @@
+const Ssb=()=>{
+    return(
+        <div className="ssb_Panel">
+            <div className="left_panel">
+                <div className="question">
+                    <div className="answers"></div>
+                </div>
+            </div>
+            <div className="right_panel"></div>
+        </div>
+    );
+};
+
+export default Ssb;
